b3c27e6f979340f891bda5d3b156b8598cc8585b
asanahmadli <asenahmedli@gmail.com>
2024-03-27 05:45:40 -0400

    I completed PriorityDeque

--- gettestie/src/test/java/rocks/zipcode/TestArrayList.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayList.java
@@ -1,4 +1,72 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class TestArrayList {
+
+    @Test
+    public void add(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("BMW");
+        arrayList.add("Subaru");
+        int size = 2;
+        Assert.assertEquals(size,arrayList.size());
+    }
+
+    @Test
+    public void set(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("BMW");
+        arrayList.set(0,"Subaru");
+        String expected  = "Subaru";
+        Assert.assertEquals(expected,arrayList.get(0));
+    }
+
+    @Test
+    public void contains(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Subaru");
+        String expected  = "Subaru";
+        Assert.assertTrue(expected,arrayList.contains("Subaru"));
+    }
+
+    @Test
+    public void remove(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("BMW");
+        arrayList.add("Subaru");
+        arrayList.remove(1);
+       String expected = "Subaru";
+        Assert.assertFalse(expected,arrayList.contains("Subaru"));
+    }
+
+    @Test
+    public void clear(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("BMW");
+        arrayList.add("Subaru");
+        arrayList.clear();
+        boolean expected = true;
+        Assert.assertEquals(expected,arrayList.isEmpty());
+    }
+
+    @Test
+    public void iterate(){
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add("BMW");
+            arrayList.add("Subaru");
+
+        Iterator<String> iterator = arrayList.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+    }
+
 }
--- gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
+++ gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
@@ -1,4 +1,64 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
 public class TestPriorityQueue {
+
+    @Test
+    public void add(){
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        que.add(1);
+        que.add(2);
+        Assert.assertTrue(que.contains(1));
+    }
+
+    @Test
+    public void poll(){
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        que.add(1);
+        que.add(2);
+        //remove element from head
+        int removed = que.poll();
+        Assert.assertFalse(que.contains(1));
+    }
+
+    @Test
+    public void peek(){
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        que.add(1);
+        que.add(2);
+        //call element from head
+        int removed = que.peek();
+        Assert.assertTrue(que.contains(1));
+    }
+
+
+    @Test
+    public void clear(){
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        que.add(1);
+        que.add(2);
+        //clear que
+        que.clear();
+        Assert.assertTrue(que.isEmpty());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
