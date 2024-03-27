37a9dc1198fe377dd00cddf4b66fd2c01bfa4e3d
asanahmadli <asenahmedli@gmail.com>
2024-03-27 06:20:53 -0400

    I completed Vector

--- gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
+++ gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
@@ -1,5 +1,60 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 public class TestLinkedList {
-    
+
+    @Test
+    public void add(){
+        LinkedList<String> linkedList = new LinkedList<>();
+        // Adding elements to the LinkedList
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Orange");
+        linkedList.add(0,"BMW");
+        String expected = "BMW";
+        Assert.assertEquals(expected,linkedList.get(0));
+    }
+
+    @Test
+    public void get(){
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        // Adding elements to the LinkedList
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Orange");
+        String expected = "Apple";
+        Assert.assertEquals(expected,linkedList.get(0));
+    }
+
+    @Test
+    public void clear(){
+        LinkedList<String> linkedList = new LinkedList<>();
+        // Adding elements to the LinkedList
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Orange");
+        linkedList.clear();
+        Assert.assertTrue(linkedList.isEmpty());
+    }
+
+
+    @Test
+    public void remove(){
+        LinkedList<String> linkedList = new LinkedList<>();
+        // Adding elements to the LinkedList
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Orange");
+        //if you dont give index then it will remove first element from head
+        linkedList.remove(0);
+        Assert.assertFalse(linkedList.contains("Apple"));
+    }
+
+
+
 }
--- gettestie/src/test/java/rocks/zipcode/TestVector.java
+++ gettestie/src/test/java/rocks/zipcode/TestVector.java
@@ -1,4 +1,52 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Vector;
+
 public class TestVector {
+    @Test
+    public void add(){
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        vector.addElement(3);
+        Assert.assertTrue(vector.contains(3));
+    }
+
+    @Test
+    public void remove(){
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        //you can pass obj
+        vector.removeElement(3);
+        //you can pass index
+        //vector.removeElementAt(2);
+        Assert.assertFalse(vector.contains(3));
+    }
+
+    @Test
+    public void elementAt(){
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        //you can pass obj
+        int actual = vector.elementAt(1);
+        int expected = 2;
+        Assert.assertEquals(expected,actual);
+        //vector dfault capacity is 10 even there less than 10 elements
+        Assert.assertEquals(10,vector.capacity());
+    }
+
+    @Test
+    public void isEmpty(){
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        vector.clear();
+        Assert.assertTrue(vector.isEmpty());
+    }
+
 }
