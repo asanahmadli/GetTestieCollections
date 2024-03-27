5cc684cfdd03a8783aa0ba04992cb1b5ad51099c
asanahmadli <asenahmedli@gmail.com>
2024-03-27 06:28:04 -0400

    I completed Stack

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,5 +1,8 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
@@ -20,5 +23,37 @@ public class TestStack {
         assertEquals(false, stack.isEmpty()); // false
     }
 
+    @Test
+    public void push(){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        int expected = 2;
+        //return element without remove
+        int actual = stack.peek();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void pop(){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        int expected = 2;
+        //return element with removing
+        int actual = stack.pop();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void empty(){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.clear();
+        int actual = stack.search(0);
+        int expected = -1;
+        Assert.assertEquals(expected,actual);
+    }
 
 }
